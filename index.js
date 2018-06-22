3bb76b5704e75eb9b719118868a77b7e6312d2b3
abbylee1 <ablee@micds.org>
2018-06-22 14:13:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,7 @@
 function scruberGreetingForFeet(feet) ;
   if (feet <= 400) {
     console.log("You get a free sample");
+  else if (feet > 400) {
+    console.log("You do not get a free sample");
+  else 
   }
