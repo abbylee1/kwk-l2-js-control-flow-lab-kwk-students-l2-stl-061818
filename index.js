cb78e7eadcd46be3dad535b5f7954090586098fb
abbylee1 <ablee@micds.org>
2018-06-22 14:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // Write your code in this file!
 function scruberGreetingForFeet(feet) ;
   if (feet <= 400)
-    console.log("You get a free sample")
+    console.log("You get a free sample");
+    
