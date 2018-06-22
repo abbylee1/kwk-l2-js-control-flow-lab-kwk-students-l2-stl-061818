115cfcb2983b2d4cd03009aa80d278b0da85e4fe
abbylee1 <ablee@micds.org>
2018-06-22 14:15:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function scruberGreetingForFeet(feet) ;
   if (feet <= 400) {
     console.log('This one is on me!');
   else if (feet > 2000) {
-    console.log("You do not get a free sample");
+    console.log("");
   else {
     console.log("Input a real foot")
   }
