7e3094168e46e433acf3662bddc3d333f4696345
abbylee1 <ablee@micds.org>
2018-06-22 14:16:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function scruberGreetingForFeet(feet) ;
   if (feet <= 400) {
     return 'This one is on me!' ;
-  else if (feet > 2000) {
+  else if (1999 < feet && feet < 2500) {
     return 'I will gladly take your thirty bucks.';
   else {
     return 'No can do.'
