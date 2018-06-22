421cb2036833391721def635c8321791d2cd40a6
abbylee1 <ablee@micds.org>
2018-06-22 14:16:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Write your code in this file!
 function scruberGreetingForFeet(feet) ;
   if (feet <= 400) {
-    console.log('This one is on me!');
+    return 'This one is on me!' ;
   else if (feet > 2000) {
-    console.log('I will gladly take your thirty bucks.');
+    return 'I will gladly take your thirty bucks.';
   else {
-    console.log('No can do.')
+    return 'No can do.'
   }
   }
